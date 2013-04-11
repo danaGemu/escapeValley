6fab1f9b911ec3c1e4cf621c8f2478ace00b3e6d
Dana Putra Kharisma <d.only99@gmail.com>
2013-04-11 17:19:43 +0700

    add score object

---
+++ ScoreObj.js
@@ -0,0 +1,12 @@
+﻿var ScoreObj = me.HUD_Item.extend(
+    {
+        init: function (x, y) {
+            this.parent(x, y);
+            this.font = new me.Font('Georgia', 24, 'yellow');
+        },
+
+        draw: function (context, x, y) {
+            this.font.draw(context, this.value, this.pos.x + x, this.pos.y + y);
+        },
+    });
+    
